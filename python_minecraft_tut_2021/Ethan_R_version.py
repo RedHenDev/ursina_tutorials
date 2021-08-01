b11434e3e440097f4353fb81e9892b01a82565c2
RedHenDev <neobn@hotmail.com>
2021-08-01 12:12:32 +0100

    Used Ethan's build_distance code
    This was a great idea!

--- python_minecraft_tut_2021/Ethan_R_version.py
+++ python_minecraft_tut_2021/Ethan_R_version.py
@@ -1,28 +1,37 @@
 """
-Minecraft in Python, with Ursina, tut 7
+Minecraft in Python, with Ursina, tut 8
+Petter Amland :)
 
-DONE -1) Theta reset!
-DONE 0) Combine subsets into megasets
-DONE 1) Create our own subCube model, with texture
-DONE 2) rotate subCubes for better texture distribution
-DONE 3) colours!
-4) More efficient terrain generation
+DONE 0) Colour correction
+DONE 1) 3.6.0 terrain issues - model and texture loading
+DONE 2) More efficient terrain generation
+DONE 3) New gravity system - better performance, no glitching, 
+    and ability to step up blocks easily (+height bounds; jump?)
+3.1) check build tool position
+4) Caves
+5) Layers of terrain
+6) Axe model
+7) Fog darkens/changes colour as we descend height
+8) Trees
+DONE 9) Tidy numpy imports
+DONE 10) The new repo - home of tutorial code
 ...
 future) Mining? 
 """
 
 from random import randrange
-from numpy.core.shape_base import block
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-from numpy import floor
-from numpy import abs
-from numpy import sin
-from numpy import cos
-from numpy import radians
+from numpy import floor,abs,sin,cos,radians
 import time
 from perlin_noise import PerlinNoise  
 from nMap import nMap
+# NB this import is different from youtube tutorial video.
+# In the 3.6.0 update for ursina, a minor change in the
+# combine() function may cause the terrain to generate
+# in pieces instead of as expected. This safe_combine()
+# function simply uses the legacy code from 3.5.0.
+from safe_combine import safe_combine
 
 app = Ursina()
 
@@ -32,20 +41,20 @@ window.exit_button.visible = False
 prevTime = time.time()
 
 scene.fog_color = color.rgb(0,222,0)
-scene.fog_density = 0.004
+scene.fog_density = 0.02
 
-grassStrokeTex = load_texture('grass_14.png')
-monoTex = load_texture('stroke_mono.png')
-wireTex = load_texture('wireframe.png')
-stoneTex = load_texture('grass_mono.png')
+grassStrokeTex = 'grass_14.png'
+monoTex = 'stroke_mono.png'
+wireTex = 'wireframe.png'
+stoneTex = 'grass_mono.png'
 
-# cubeTex = load_texture('block_texture.png')
-# cubeModel = load_model('moonCube.obj')
-cubeTex = load_texture('hexTex.png')
-cubeModel = load_model('basic_hex.obj')
+cubeTex = 'block_texture.png'
 
-axoTex = load_texture('axolotl.png')
-axoModel = load_model('axolotl.obj')
+cubeModel = 'moonCube'
+
+
+axoTex = 'axolotl.png'
+axoModel = 'axolotl.obj'
 
 bte = Entity(model='cube',texture=wireTex)
 class BTYPE:
@@ -56,14 +65,17 @@ class BTYPE:
 
 blockType = BTYPE.SOIL
 buildMode = -1  # -1 is OFF, 1 is ON.
+build_distance = 3
 
 def buildTool():
+    global build_distance
     if buildMode == -1:
         bte.visible = False
         return
     else: bte.visible = True
-    bte.position = round(subject.position +
-                    camera.forward * 3)
+    bte.position = round(   subject.position +
+                            camera.forward *
+                            build_distance )
     bte.y += 2
     bte.y = round(bte.y)
     bte.x = round(bte.x)
@@ -78,12 +90,18 @@ def build():
 
 def input(key):
     global blockType, buildMode, generating
-    global canGenerate
+    global canGenerate, build_distance
     if key == 'q' or key == 'escape':
         quit()
     if key == 'g': 
         generating *= -1
         canGenerate *= -1
+    
+    # E.R. scroll down to build closer or scroll up to build further
+    if key == 'scroll up':
+        build_distance += 1
+    if key == 'scroll down':
+        build_distance -= 1
 
     if buildMode == 1 and key == 'left mouse up':
         build()
@@ -101,16 +119,9 @@ def input(key):
 def update():
     global prevZ, prevX, prevTime, genSpeed, perCycle
     global rad, origin, generating, canGenerate
-    global iterations, toIterate, currentVec
-    global changes, subPos
     if  abs(subject.z - prevZ) > 1 or \
         abs(subject.x - prevX) > 1:
-            subPos.x=floor(subject.x)
-            subPos.y=floor(subject.z)
-            currentVec = 0
-            iterations = 0
-            toIterate = 1
-            changes = -1
+            origin=subject.position
             rad=0
             theta=0
             generating = 1 * canGenerate
@@ -138,11 +149,11 @@ subCubes = []
 # New variables :)
 generating = 1 # -1 if off.
 canGenerate = 1 # -1 if off.
-genSpeed = 0.01
+genSpeed = 0
 perCycle = 64
 currentCube = 0
 currentSubset = 0
-numSubCubes = 32
+numSubCubes = 64
 numSubsets = 420 # I.e. how many combined into a megaset?
 theta = 0
 rad = 0
@@ -150,30 +161,22 @@ rad = 0
 # at location specified in key.
 subDic = {}
 
-# For new position of subset.
-currentVec = 0
-iterations = 0
-toIterate = 1
-changes = -1
-subPos = Vec2(0,0)
-swirlVecs = [
-    Vec2(0,0),
-    Vec2(0,1),
-    Vec2(1,0),
-    Vec2(0,-1),
-    Vec2(-1,0)
-]
+caveDic = { 'x9z9':'cave',
+            'x10z9':'cave',
+            'x11z9':'cave',
+            'x9z10':'cave',
+            'x9z11':'cave'}
 
 # Instantiate our 'ghost' subset cubes.
 for i in range(numSubCubes):
-    bud = Entity(model=cubeModel)
-    # bud.rotation_y = random.randint(1,4)*90
+    bud = Entity(model=cubeModel,texture=cubeTex)
+    bud.rotation_y = random.randint(1,4)*90
     bud.disable()
     subCubes.append(bud)
 
 # Instantiate our empty subsets.
 for i in range(numSubsets):
-    bud = Entity(model=None)
+    bud = Entity(model=cubeModel)
     bud.texture = cubeTex
     bud.disable()
     subsets.append(bud)
@@ -185,22 +188,24 @@ def genPerlin(_x, _z):
     y += ((noise([_x/freq,_z/freq]))*amp)
     freq = 32
     amp = 21
-    y += ((noise([(_x)/freq,(_z)/freq]))*amp)
+    y += ((noise([_x/freq,_z/freq]))*amp)
+
+    # Is there are cave-gap here?
+    # If so, lower the cube by 32...or something ;)
+    if caveDic.get('x'+str(int(_x))+'z'+str(int(_z)))=='cave':
+        y-=9
+
     return floor(y)
 
 def genTerrain():
     global currentCube, theta, rad, currentSubset
     global generating
-    global iterations, toIterate, currentVec
-    global changes, subPos
 
     if generating==-1: return
 
     # Decide where to place new terrain cube!
-    subPos.x += floor(swirlVecs[currentVec].x)
-    subPos.y += floor(swirlVecs[currentVec].y)
-    x = subPos.x
-    z = subPos.y
+    x = floor(origin.x + sin(radians(theta)) * rad)
+    z = floor(origin.z + cos(radians(theta)) * rad)
     # Check whether there is terrain here already...
     if subDic.get('x'+str(x)+'z'+str(z))!='i':
         subCubes[currentCube].enable()
@@ -209,9 +214,10 @@ def genTerrain():
         subDic['x'+str(x)+'z'+str(z)] = 'i'
         subCubes[currentCube].parent = subsets[currentSubset]
         y = subCubes[currentCube].y = genPerlin(x,z)
-        g = nMap(y,-16,16,0,220)
-        g += random.randint(-12,12)
-        subCubes[currentCube].color = color.rgb(g,g,g)
+        # OK -- time to decide colours :D
+        c = nMap(y,-8,21,132,212)
+        c += random.randint(-32,32)
+        subCubes[currentCube].color = color.rgb(c,c,c)
         subCubes[currentCube].disable()
         currentCube+=1
 
@@ -221,34 +227,34 @@ def genTerrain():
             subsets[currentSubset].enable()
             currentSubset+=1
             currentCube=0
-
+            
             # And ready to build a megaset?
             if currentSubset==numSubsets:
-                megasets.append(Entity(texture=cubeTex))
+                megasets.append(Entity( model=cubeModel,
+                                        texture=cubeTex))
                 # Parent all subsets to our new megaset.
                 for s in subsets:
                     s.parent=megasets[-1]
+                # In case user has Ursina version 3.6.0.
+                # safe_combine(megasets[-1],auto_destroy=False)
                 megasets[-1].combine(auto_destroy=False)
+                for s in subsets:
+                    s.parent=scene
                 currentSubset=0
                 print('Megaset #' + str(len(megasets))+'!')
-
+            
     else:
         pass
-        # print('Bump!')
         # There was terrain already there, so
         # continue rotation to find new terrain spot.
     
-    # Co-ordinate new vector by iteration around swirl.
-    iterations+=1
-    if iterations == toIterate:
-        currentVec+=1
-        if currentVec == len(swirlVecs):
-            currentVec = 1
-        changes+=1
-        iterations = 0
-        if changes == 2:
-            changes=0
-            toIterate+=1
+    if rad > 0:
+        theta += 45/rad
+    else: rad+=0.5
+    
+    if theta >= 360:
+        theta = 0
+        rad += 0.5
 
 shellies = []
 shellWidth = 3
@@ -257,20 +263,26 @@ for i in range(shellWidth*shellWidth):
     bud.visible=False
     shellies.append(bud)
 
+# Our new gravity system for moving the subject :)
 def generateShell():
-    global shellWidth
-    for i in range(len(shellies)):
-        x = shellies[i].x = floor((i/shellWidth) + 
-                            subject.x - 0.5*shellWidth)
-        z = shellies[i].z = floor((i%shellWidth) + 
-                            subject.z - 0.5*shellWidth)
-        shellies[i].y = genPerlin(x,z)
+    global subject
+
+    target_y = genPerlin(subject.x,subject.z) + 2
+    subject.y = lerp(subject.y, target_y, 9.807*time.dt)
+
+    # global shellWidth
+    # for i in range(len(shellies)):
+    #     x = shellies[i].x = floor((i/shellWidth) + 
+    #                         subject.x - 0.5*shellWidth)
+    #     z = shellies[i].z = floor((i%shellWidth) + 
+    #                         subject.z - 0.5*shellWidth)
+    #     shellies[i].y = genPerlin(x,z)
 
 subject = FirstPersonController()
 subject.cursor.visible = False
-subject.gravity = 0.1
+subject.gravity = 0
 subject.x = subject.z = 5
-subject.y = 24
+subject.y = 32
 prevZ = subject.z
 prevX = subject.x
 origin = subject.position # Vec3 object? .x .y .z
