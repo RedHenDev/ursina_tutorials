8de998d995e8c9d2616f7a10273e05b7fef00933
RedHenDev <neobn@hotmail.com>
2021-08-29 16:28:54 +0100

    tut 13 complete! Phew!

--- python_minecraft_tut_2021/mining_system.py
+++ python_minecraft_tut_2021/mining_system.py
@@ -3,7 +3,7 @@ Build tool system for mining and building :)
 Good luck!
 """
 
-from ursina import Entity, color
+from ursina import Entity, color, texture
 from numpy import floor
 
 class Mining_system:
@@ -14,6 +14,14 @@ class Mining_system:
         # and elsewhere?
         this.subject = _subject
         this.camera = _camera
+
+        # Dictionaries for recording positions of terrain
+        # including gaps.
+        this.tDic = {}
+        this.buildTex = 'build_texture.png' 
+        this.cubeModel = 'moonCube.obj'
+        this.builds = Entity(model=this.cubeModel,
+                            texture=this.buildTex)
 
         # A reference to the terrain's subsets
         # is needed for mining etc.
@@ -34,25 +42,18 @@ class Mining_system:
 
         # Our new block type system...
         this.blockTypes = []
-        #  Stone
+        #  Stone - 0
         this.blockTypes.append(color.rgb(255,255,255))
-        # Grass
+        # Grass - 1
         this.blockTypes.append(color.rgb(0,255,0))
-        # Soil
+        # Soil - 2
         this.blockTypes.append(color.rgb(255,80,100))
-        # Ruby
+        # Ruby - 3
         this.blockTypes.append(color.rgb(255,0,0))
-        # Netherite
+        # Netherite - 4
         this.blockTypes.append(color.rgb(0,0,0))
         # Our current block type.
         this.blockType = 0 # I.e. stone.
-        
-
-    def build(this):
-        pass
-
-    def mine(this):
-        pass
 
     def input(this, key):
         # scroll down to build closer or 
@@ -94,6 +95,86 @@ class Mining_system:
         this.bte.z = round(this.bte.z)
         this.bte.color = this.blockTypes[this.blockType]
 
+    def mineSpawn(this):
+        # Spawn one block below dig position?
+        if this.tDic.get(   'x'+str(this.bte.x)+
+                            'y'+str(this.bte.y-1)+
+                            'z'+str(this.bte.z)
+                            ) == None:
+
+            # Record gap location in dictionary.
+            this.tDic[  'x'+str(this.bte.x)+
+                        'y'+str(this.bte.y)+
+                        'z'+str(this.bte.z)] = 'gap'
+                    
+            e = Entity( model=this.cubeModel,
+                        texture=this.buildTex)
+            # Shrink spawned block so that it
+            # matches the size of ordinary terrain.
+            e.scale *= 0.99999
+            # Change colour to soil (this.blockTypes[2]).
+            e.color = this.blockTypes[0]
+            # Position under mined area.
+            e.position = this.bte.position
+            e.y -= 1
+            # Parent spawned cube into builds entity.
+            e.parent = this.builds
+            # Record newly spawned block on dictionary.
+            this.tDic[  'x'+str(this.bte.x)+
+                        'y'+str(this.bte.y)+
+                        'z'+str(this.bte.z)] = e.y
+
+            # OK -- now spawn 4 'cave wall' cubes.
+            # For each cube, first check whether:
+            # 1) No terrain there already
+            # 2) No gaps
+            # 3) No terrain below this pos
+            x = this.bte.x
+            y = this.bte.y
+            z = this.bte.z
+            pos1 = (x+1,y,z)
+            pos2 = (x-1,y,z)
+            pos3 = (x,y,z+1)
+            pos4 = (x,y,z-1)
+            spawnPos = []
+            spawnPos.append(pos1)
+            spawnPos.append(pos2)
+            spawnPos.append(pos3)
+            spawnPos.append(pos4)
+            for i in range(4):
+                x = spawnPos[i][0]
+                z = spawnPos[i][2]
+                y = spawnPos[i][1]
+                    
+                # We can ask None both times because
+                # this covers both gaps and terrain
+                # being in these positions (i.e
+                # potential cave wall and below
+                # potential cave wall.
+                if this.tDic.get(   'x'+str(x)+
+                                    'y'+str(y)+
+                                    'z'+str(z)
+                        ) == None and \
+                        this.tDic.get(  'x'+str(x)+
+                                        'y'+str(y-1)+
+                                        'z'+str(z)
+                        ) == None:
+                            e = Entity( model=this.cubeModel,
+                                        texture=this.buildTex)
+                            # Shrink spawned block so that it
+                            # matches the size of ordinary terrain.
+                            e.scale *= 0.99999
+                            # Change colour to soil (this.blockTypes[2]).
+                            e.color = this.blockTypes[0]
+                            # Position around mined area.
+                            e.position = spawnPos[i]
+                            # Parent spawned cube into builds entity.
+                            e.parent = this.builds
+                            # Record newly spawned block on dictionary.
+                            this.tDic[  'x'+str(x)+
+                                        'y'+str(y)+
+                                        'z'+str(z)] = e.y
+
     # Place a block at the bte's position.
     def build(this):
         if this.buildMode == -1: return
@@ -105,14 +186,39 @@ class Mining_system:
         e.shake(duration=0.5,speed=0.01)
     
     def mine(this):
-        # e = mouse.hovered_entity
-        # destroy(e)
+
+        vChange = False
+            
+        for v in this.builds.model.vertices:
+            # Is the vertex close enough to
+            # where we want to mine (bte position)?
+            if (v[0] >=this.bte.x - 0.5 and
+                v[0] <=this.bte.x + 0.5 and
+                v[1] >=this.bte.y - 0.5 and
+                v[1] <=this.bte.y + 0.5 and
+                v[2] >=this.bte.z - 0.5 and
+                v[2] <=this.bte.z + 0.5):
+
+                # Move vertex high into air to
+                # give illusion of being destroyed.
+                v[1] = 9999
+                # Note that we have made change.
+                vChange = True
+                
+        if vChange == True:
+            this.mineSpawn()
+            this.builds.model.generate()
+            this.builds.combine()
+            # Not done! Also combine newly spawned blocks
+            # into builds entity :)
+            return  
 
         # Our real mining of the terrain :)
         # Iterate over all the subsets that we have...
+        totalV = 0
         for s in range(len(this.subsets)):
             vChange = False
-            totalY = 0
+            
             for v in this.subsets[s].model.vertices:
                 # Is the vertex close enough to
                 # where we want to mine (bte position)?
@@ -123,13 +229,31 @@ class Mining_system:
                     v[2] >=this.bte.z - 0.5 and
                     v[2] <=this.bte.z + 0.5):
                     # Yes!
-                    # v[1] -= 1
+                    #v[1] -= 1
+                    # Move vertex high into air to
+                    # give illusion of being destroyed.
                     v[1] = 9999
                     # Note that we have made change.
                     # Gather average height for cave dic.
                     vChange = True
+                    totalV += 1
+                    # The mystery of 36 vertices!! :o
+                    # print('tV= ' + str(totalV))
+                    if totalV==36: break
             
             if vChange == True:
+
+                # Now we need to spawn a new cube below
+                # the bte's position -- if no cube or
+                # gap there already.
+                # Next, spawn 4 cubes to create illusion
+                # of more layers -- if each position is
+                # neither a gap nor a place where terrain
+                # already is.
+
+                this.mineSpawn()
+                # Now that we've spawned what (if anything)
+                # we need to, update subset model. Done.
                 this.subsets[s].model.generate()
+                this.builds.combine()
                 return
-                # anush.makeCave(bte.x,bte.z,bte.y-1)  
