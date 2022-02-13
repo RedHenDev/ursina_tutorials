1e5543fc86f0e1f17d78433792cf6b54c055c84b
RedHenDev <neobn@hotmail.com>
2022-02-13 19:31:51 +0000

    perpendicular building nearly working

---
+++ python_meshCraft_tut_2021/main_PREP.py
@@ -0,0 +1,100 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from mesh_terrain_PREP import MeshTerrain
+from flake import SnowFall
+import random as ra
+from bump_system import *
+from save_load_system import saveMap, loadMap
+
+app = Ursina()
+
+window.color = color.rgb(0,200,255)
+indra = Sky()
+indra.color = window.color
+subject = FirstPersonController()
+subject.gravity = 0.0
+subject.cursor.visible=True
+subject.cursor.color=color.white
+subject.frog=False # For jumping...
+subject.runSpeed=12
+subject.walkSpeed=4
+camera.dash=10 # Rate at which fov changes when running.
+window.fullscreen=False
+
+terrain = MeshTerrain()
+# ***
+terrain.generatingTerrain=True
+# snowfall = SnowFall(subject)
+# *** 
+scene.fog_density=(0,50)
+scene.fog_color=indra.color
+
+for i in range(32):
+    terrain.genTerrain()
+
+grass_audio = Audio('step.ogg',autoplay=False,loop=False)
+snow_audio = Audio('snowStep.mp3',autoplay=False,loop=False)
+
+pX = subject.x
+pZ = subject.z
+
+def input(key):
+    terrain.input(key)
+    if key=='g':
+        terrain.generatingTerrain = not terrain.generatingTerrain
+    # Jumping...
+    if key=='space': subject.frog=True
+    # Saving and loading...
+    if key=='m': saveMap(subject.position,terrain.td)
+    if key=='l': loadMap(subject,terrain)
+
+count = 0
+def update():
+    global count, pX, pZ
+
+    # Highlight terrain block for mining/building...
+    terrain.update(subject.position,camera)
+
+    # Handle mob ai.
+    mob_movement(grey, subject.position, terrain.td)
+
+    count+=1
+    if count == 4:
+        
+        count=0
+        # Generate terrain at current swirl position.
+        if terrain.generatingTerrain:
+            for i in range(4):
+                terrain.genTerrain()
+        
+
+    # Change subset position based on subject position.
+    if abs(subject.x-pX)>1 or abs(subject.z-pZ)>1:
+        pX=subject.x
+        pZ=subject.z 
+        terrain.swirlEngine.reset(pX,pZ)
+        # Sound :)
+        if subject.y > 4:
+            if snow_audio.playing==False:
+                snow_audio.pitch=ra.random()+0.25
+                snow_audio.play()
+        elif grass_audio.playing==False:
+            grass_audio.pitch=ra.random()+0.7
+            grass_audio.play()
+    
+    # Walk on solid terrain, and check wall collisions.
+    bumpWall(subject,terrain)
+    # Running and dash effect.
+    if held_keys['shift'] and held_keys['w']:
+        subject.speed=subject.runSpeed
+        if camera.fov<100:
+            camera.fov+=camera.dash*time.dt
+    else:
+        subject.speed=subject.walkSpeed
+        if camera.fov>90:
+            camera.fov-=camera.dash*4*time.dt
+            if camera.fov<90:camera.fov=90
+
+from mob_system import *
+
+app.run()
---
+++ python_meshCraft_tut_2021/mining_system_PREP.py
@@ -0,0 +1,47 @@
+from ursina import Entity, color, floor, Vec3
+# ***
+bte = Entity(model='block.obj',color=color.rgba(1,1,0,0.4))
+bte.scale=1.001
+bp = Entity(model='block.obj',color=color.rgba(0,0,1,0.4))
+bp.scale=1.001
+# ***
+def highlight(pos,cam,td,fbs=False):
+    for i in range(1,32):
+        # Adjust for player's height!
+        wp=pos+Vec3(0,1.86,0)+cam.forward*(i*0.5)
+        # This trajectory is close to perfect!
+        # If we can hit perfection...one day...?
+        x = round(wp.x)
+        y = floor(wp.y)
+        z = round(wp.z)
+        bte.x = x
+        bte.y = y # ***
+        bte.z = z
+        if td.get((x,y,z))=='t':
+            bte.visible = True
+            # ***
+            if fbs==True:
+                return i
+            break
+        else:
+            bte.visible = False
+
+def mine(td,vd,subsets):
+    if not bte.visible: return
+    # ***
+    wv=vd.get((int(bte.x),int(bte.y),int(bte.z)))
+    
+    # Have we got a block highlighted? If not, return.
+    if wv==None: return
+    
+    for i in range(wv[1]+1,wv[1]+37):
+        subsets[wv[0]].model.vertices[i][1]+=999
+    
+    subsets[wv[0]].model.generate()
+
+    # g for gap in terrain. And wipe vd entry.
+    # ***
+    td[ (int(bte.x),int(bte.y),int(bte.z))]='g'
+    vd[ (int(bte.x),int(bte.y),int(bte.z))] = None
+    # ***
+    return (bte.position, wv[0])
